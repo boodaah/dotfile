b5287a70e61d1aab5b654798089d157bc6d6eb78
boodaah <boodaah@me.com>
2015-03-21 23:26:40 -0400

    Update terminal settings

--- settings.sh
+++ settings.sh
@@ -17,8 +17,10 @@ defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
 defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
 
 # Set terminal window size 
-/usr/libexec/PlistBuddy -c "Add :'Window Settings':Pro:rowCount integer 40" ~/Library/Preferences/com.apple.Terminal.plist
-/usr/libexec/PlistBuddy -c "Add :'Window Settings':Pro:columnCount integer 160" ~/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Add :'Window Settings':Pro:rowCount integer" ~/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:rowCount 40" ~/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Add :'Window Settings':Pro:columnCount integer" ~/Library/Preferences/com.apple.Terminal.plist
+/usr/libexec/PlistBuddy -c "Set :'Window Settings':Pro:columnCount 160" ~/Library/Preferences/com.apple.Terminal.plist
 
 CFPreferencesAppSynchronize "com.apple.Terminal"
 
