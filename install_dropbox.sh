936e2f488999677e2f85ef09ce58500fea856aa0
boodaah <boodaah@me.com>
2015-10-02 07:01:18 -0400

    Create install_dropbox.sh

---
+++ install_dropbox.sh
@@ -0,0 +1,5 @@
+# ==============================================
+# Alternate to 'brew cask home dropbox'
+# ==============================================
+
+curl -L -G -d full=1 -d plat=mac -o DropboxInstaller.dmg https://www.dropbox.com/download
